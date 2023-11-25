04c909bd70d031629376726b4e3d4c7e061a6620
Haze2910 <40241078+Haze2910@users.noreply.github.com>
2023-11-25 16:38:27 +0100

    Update RBFFeatureEncoder.py

--- RBFFeatureEncoder.py
+++ RBFFeatureEncoder.py
@@ -5,11 +5,21 @@ class RBFFeatureEncoder:
         self.centers = centers
         self.sigma = sigma
 
-    def encode(self, state): # modify
-        # Calculate the norm between the state and the centers and return the RBF encoded features
-        norms = np.linalg.norm(state - centers), axis=1) ** 2 
-        return (np.exp( (-norms/(2 * self.sigma ** 2)) )).flatten()
+    def encode(self, x):
+        """
+        Calculate the norm between the inputs and the centers and return the RBF encoded features for each input i.e 
+        
+            out_i = exp( -(||x - c_i|| ** 2) / 2 * sigma ** 2)
+            
+        Input: (n, input_size)
+        Output: (n, centers.shape[0])
+        """
+        norms = np.linalg.norm((x[:, np.newaxis, :] - self.centers), axis=2) ** 2 
+        return (np.exp((-norms / (2 * self.sigma ** 2))))
 
     @property
-    def size(self): 
+    def size(self):
+        """
+        number of output features i.e number of centers
+        """
         return self.centers.shape[0]
