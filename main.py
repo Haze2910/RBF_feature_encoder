db93f8e4499ab28591ec48977577a365307d6cf2
Haze2910 <40241078+Haze2910@users.noreply.github.com>
2023-11-25 16:45:00 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import numpy as np
+import BatchNormalizer, RBFFeatureEncoder
+
+if __name__ == "__main__":
+
+    x_dim = 5 # dimension of the data
+  
+    n_samples = 10 # number of samples to calculate mean and std of
+    samples = np.random.random((n_samples, x_dim)
+
+    # Initialize the normalizer and fit the samples
+    normalizer = BatchNormalizer()
+    normalizer.fit(samples)
+
+    n_data = 20 # number of data points
+    x = np.random.random((n_data, x_dim))
+
+    # Normalize the data
+    normalized_x = normalizer.transform(x) 
+
+    n_centers = 10 # number of centers i.e. number of output feature for each data point
+    centers = np.random.random((n_centers, x_dim))
+
+    # Initialize the encoder
+    encoder = RBFFeatureEncoder(centers)
+
+    # Encode each data point
+    encoded_x = encoder.encode(normalized_x)
+    print(f"Encoded data:\n {encoded_x}")
+    
